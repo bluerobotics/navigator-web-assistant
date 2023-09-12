9978befb53b3a8292da6a24e773051623dac0365
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-09-12 15:57:14 -0300

    src: hardware_manager: Add pwm related functions
    src: server: fix bind route, fix CORs issue
    
    src: server: package: minor fix on sensor type
    
    src: server: package: add pwm package handlers
    
    src: server: package: add pwm_channel from string handler
    
    src: server: rest: rework on get_sensors method
    
    src: server: rest: add pwm related post services
    
    src: server: add pwm related post services
    
    src: server: change sensors service name

--- src/server/protocols/v1/rest.rs
+++ src/server/protocols/v1/rest.rs
@@ -1,4 +1,4 @@
-use crate::server::protocols::v1::packages::package;
+use crate::{hardware_manager, server::protocols::v1::packages::package};
 use actix_web::{get, post, web, HttpResponse, Responder};
 use std::str::FromStr;
 
@@ -13,7 +13,32 @@ async fn echo(req_body: String) -> impl Responder {
 }
 
 #[get("v1/sensor/{sensor}")]
-async fn sensor(sensor: web::Path<String>) -> impl Responder {
-    let package = package::reading(package::Sensors::from_str(sensor.as_str()).unwrap());
+async fn get_sensor(sensor: web::Path<String>) -> impl Responder {
+    let package = package::reading(package::Sensors::from_str(&sensor.into_inner()).unwrap());
+    HttpResponse::Ok().json(package)
+}
+
+#[post("v1/actuator/pwm/{channel}/{value}")]
+async fn post_pwm(path: web::Path<(String, u16)>) -> impl Responder {
+    let (channel, value) = path.into_inner();
+    let package = package::pwm_channel_value(
+        hardware_manager::PwmChannel::from_str(channel.as_str()).unwrap(),
+        value,
+    );
+    HttpResponse::Ok().json(package)
+}
+
+#[post("v1/actuator/pwm/enable/{bool}")]
+async fn post_pwm_enable(path: web::Path<bool>) -> impl Responder {
+    let bool = path.into_inner();
+    let package = package::pwm_enable(bool);
+    HttpResponse::Ok().json(package)
+}
+
+#[post("v1/actuator/pwm/frequency/{frequency}")]
+async fn post_pwm_frequency(path: web::Path<f32>) -> impl Responder {
+    let frequency = path.into_inner();
+    let package = package::set_pwm_freq_hz(frequency);
+    hardware_manager::pwm_enable(true);
     HttpResponse::Ok().json(package)
 }
