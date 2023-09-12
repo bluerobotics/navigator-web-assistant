37bc8a609647a47cc6e91342841a09f49ce52f5f
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-09-12 16:02:55 -0300

    src: server: package: add pwm_channel from string handler

--- src/server/protocols/v1/packages.rs
+++ src/server/protocols/v1/packages.rs
@@ -36,6 +36,35 @@ pub mod package {
         }
     }
 
+    impl FromStr for hardware_manager::PwmChannel {
+        type Err = Box<dyn Error>;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let s = s.to_lowercase();
+
+            match s.as_str() {
+                "ch1" => Ok(hardware_manager::PwmChannel::Ch1),
+                "ch2" => Ok(hardware_manager::PwmChannel::Ch2),
+                "ch3" => Ok(hardware_manager::PwmChannel::Ch3),
+                "ch4" => Ok(hardware_manager::PwmChannel::Ch4),
+                "ch5" => Ok(hardware_manager::PwmChannel::Ch5),
+                "ch6" => Ok(hardware_manager::PwmChannel::Ch6),
+                "ch7" => Ok(hardware_manager::PwmChannel::Ch7),
+                "ch8" => Ok(hardware_manager::PwmChannel::Ch8),
+                "ch9" => Ok(hardware_manager::PwmChannel::Ch9),
+                "ch10" => Ok(hardware_manager::PwmChannel::Ch10),
+                "ch11" => Ok(hardware_manager::PwmChannel::Ch11),
+                "ch12" => Ok(hardware_manager::PwmChannel::Ch12),
+                "ch13" => Ok(hardware_manager::PwmChannel::Ch13),
+                "ch14" => Ok(hardware_manager::PwmChannel::Ch14),
+                "ch15" => Ok(hardware_manager::PwmChannel::Ch15),
+                "ch16" => Ok(hardware_manager::PwmChannel::Ch16),
+                "all" => Ok(hardware_manager::PwmChannel::All),
+                _ => Err(format!("{} is not a valid PwmChannel variant", s).into()),
+            }
+        }
+    }
+
     pub fn pwm_channel_value(channel: hardware_manager::PwmChannel, value: u16) -> String {
         hardware_manager::set_pwm_channel_value(channel, value);
         "success".to_string()
