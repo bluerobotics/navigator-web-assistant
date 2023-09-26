12a27fdc123e21208697af767f83ae1f0fd7e5d9
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-09-26 09:07:47 -0300

    src: server: structures: update to use input & output based variable names
    src: server: packages: update to use input & output based structures
    
    src: server: rest: update to use input & output to create routes
    
    src: server: frontend: update rest requests and json to use new input & output convention
    
    src: server: structures: add server metadata structure for BlueOS extensions
    
    src: server: rest: add get register_service route for BlueOS
    
    src: server: add get register_service route for BlueOS
    
    src: server: frontend: change html title

--- src/server/protocols/v1/rest.rs
+++ src/server/protocols/v1/rest.rs
@@ -1,4 +1,7 @@
-use crate::{hardware_manager, server::protocols::v1::packages};
+use crate::{
+    hardware_manager,
+    server::protocols::v1::{packages, structures::ServerMetadata},
+};
 use actix_web::{get, post, web, HttpResponse, Responder};
 use mime_guess::from_path;
 use std::{str::FromStr, vec};
@@ -37,20 +40,20 @@ async fn init() -> impl Responder {
     HttpResponse::Ok().json(package)
 }
 
-#[get("v1/sensor/{sensor}")]
+#[get("v1/input/{sensor}")]
 async fn get_sensor(sensor: web::Path<String>) -> impl Responder {
     let package = packages::reading(packages::Sensors::from_str(&sensor.into_inner()).unwrap());
     HttpResponse::Ok().json(package)
 }
 
-#[get("v1/actuator/userled/{userled}")]
+#[get("v1/output/userled/{userled}")]
 async fn get_led(userled: web::Path<String>) -> impl Responder {
     let package =
         packages::get_led(hardware_manager::UserLed::from_str(&userled.into_inner()).unwrap());
     HttpResponse::Ok().json(package)
 }
 
-#[post("v1/actuator/userled/{userled}/{value}")]
+#[post("v1/output/userled/{userled}/{value}")]
 async fn post_led(path: web::Path<(String, bool)>) -> impl Responder {
     let (userled, value) = path.into_inner();
     let package = packages::set_led(
@@ -60,14 +63,14 @@ async fn post_led(path: web::Path<(String, bool)>) -> impl Responder {
     HttpResponse::Ok().json(package)
 }
 
-#[post("v1/actuator/neopixel/{red}/{green}/{blue}")]
+#[post("v1/output/neopixel/{red}/{green}/{blue}")]
 async fn post_neopixel(path: web::Path<(u8, u8, u8)>) -> impl Responder {
     let (red, green, blue) = path.into_inner();
     let package = packages::set_neopixel(vec![[red, green, blue]]);
     HttpResponse::Ok().json(package)
 }
 
-#[post("v1/actuator/pwm/{channel}/{value}")]
+#[post("v1/output/pwm/{channel}/{value}")]
 async fn post_pwm(path: web::Path<(String, u16)>) -> impl Responder {
     let (channel, value) = path.into_inner();
     let package = packages::pwm_channel_value(
@@ -77,17 +80,24 @@ async fn post_pwm(path: web::Path<(String, u16)>) -> impl Responder {
     HttpResponse::Ok().json(package)
 }
 
-#[post("v1/actuator/pwm/enable/{bool}")]
+#[post("v1/output/pwm/enable/{bool}")]
 async fn post_pwm_enable(path: web::Path<bool>) -> impl Responder {
     let bool = path.into_inner();
     let package = packages::pwm_enable(bool);
     HttpResponse::Ok().json(package)
 }
 
-#[post("v1/actuator/pwm/frequency/{frequency}")]
+#[post("v1/output/pwm/frequency/{frequency}")]
 async fn post_pwm_frequency(path: web::Path<f32>) -> impl Responder {
     let frequency = path.into_inner();
     let package = packages::set_pwm_freq_hz(frequency);
     hardware_manager::pwm_enable(true);
+    HttpResponse::Ok().json(package)
+}
+
+/// The "register_service" route is used by BlueOS extensions manager
+#[get("register_service")]
+async fn get_server_metadata() -> impl Responder {
+    let package = ServerMetadata::default();
     HttpResponse::Ok().json(package)
 }
