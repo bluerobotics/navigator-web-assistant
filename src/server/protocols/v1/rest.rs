d8d25f1debb800ab6640ed107f4109ddbc3adc0b
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-10-04 15:36:11 -0300

    src: server: v1: rest: Add paperclip and it's tag to all rest routes

--- src/server/protocols/v1/rest.rs
+++ src/server/protocols/v1/rest.rs
@@ -2,8 +2,12 @@ use crate::{
     hardware_manager,
     server::protocols::v1::{packages, structures::ServerMetadata},
 };
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::Responder;
 use mime_guess::from_path;
+use paperclip::actix::{
+    api_v2_operation, get, post,
+    web::{self, HttpResponse},
+};
 use std::{str::FromStr, vec};
 
 #[derive(rust_embed::RustEmbed)]
@@ -18,41 +22,41 @@ fn handle_embedded_file(path: &str) -> HttpResponse {
         None => HttpResponse::NotFound().body("404 Not Found"),
     }
 }
-
+#[api_v2_operation]
 #[get("/")]
 async fn index() -> impl Responder {
     handle_embedded_file("index.html")
 }
-
+#[api_v2_operation]
 #[get("/dist/{_:.*}")]
 async fn dist(path: web::Path<String>) -> impl Responder {
     handle_embedded_file(path.as_str())
 }
-
+#[api_v2_operation]
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
-
+#[api_v2_operation]
 #[get("v1/settings/init")]
 async fn init() -> impl Responder {
     let package = packages::init();
     HttpResponse::Ok().json(package)
 }
-
+#[api_v2_operation]
 #[get("v1/input/{sensor}")]
 async fn get_sensor(sensor: web::Path<String>) -> impl Responder {
     let package = packages::reading(packages::Sensors::from_str(&sensor.into_inner()).unwrap());
     HttpResponse::Ok().json(package)
 }
-
+#[api_v2_operation]
 #[get("v1/output/userled/{userled}")]
 async fn get_led(userled: web::Path<String>) -> impl Responder {
     let package =
         packages::get_led(hardware_manager::UserLed::from_str(&userled.into_inner()).unwrap());
     HttpResponse::Ok().json(package)
 }
-
+#[api_v2_operation]
 #[post("v1/output/userled/{userled}/{value}")]
 async fn post_led(path: web::Path<(String, bool)>) -> impl Responder {
     let (userled, value) = path.into_inner();
@@ -62,14 +66,14 @@ async fn post_led(path: web::Path<(String, bool)>) -> impl Responder {
     );
     HttpResponse::Ok().json(package)
 }
-
+#[api_v2_operation]
 #[post("v1/output/neopixel/{red}/{green}/{blue}")]
 async fn post_neopixel(path: web::Path<(u8, u8, u8)>) -> impl Responder {
     let (red, green, blue) = path.into_inner();
     let package = packages::set_neopixel(vec![[red, green, blue]]);
     HttpResponse::Ok().json(package)
 }
-
+#[api_v2_operation]
 #[post("v1/output/pwm/{channel}/{value}")]
 async fn post_pwm(path: web::Path<(String, u16)>) -> impl Responder {
     let (channel, value) = path.into_inner();
@@ -79,7 +83,7 @@ async fn post_pwm(path: web::Path<(String, u16)>) -> impl Responder {
     );
     HttpResponse::Ok().json(package)
 }
-
+#[api_v2_operation]
 #[post("v1/output/pwm/enable/{bool}")]
 async fn post_pwm_enable(path: web::Path<bool>) -> impl Responder {
     let bool = path.into_inner();
@@ -87,6 +91,7 @@ async fn post_pwm_enable(path: web::Path<bool>) -> impl Responder {
     HttpResponse::Ok().json(package)
 }
 
+#[api_v2_operation]
 #[post("v1/output/pwm/frequency/{frequency}")]
 async fn post_pwm_frequency(path: web::Path<f32>) -> impl Responder {
     let frequency = path.into_inner();
@@ -96,6 +101,7 @@ async fn post_pwm_frequency(path: web::Path<f32>) -> impl Responder {
 }
 
 /// The "register_service" route is used by BlueOS extensions manager
+#[api_v2_operation]
 #[get("register_service")]
 async fn get_server_metadata() -> impl Responder {
     let package = ServerMetadata::default();
