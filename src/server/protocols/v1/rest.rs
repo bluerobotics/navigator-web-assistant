ae632b3553f0c073fc01cf3c65dc0c4e422f7d3c
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-10-09 16:29:51 -0300

    src: server: v1: rest: Fix - add required apiv2 response to routes

--- src/server/protocols/v1/rest.rs
+++ src/server/protocols/v1/rest.rs
@@ -1,12 +1,15 @@
 use crate::{
     hardware_manager,
-    server::protocols::v1::{packages, structures::ServerMetadata},
+    server::protocols::v1::{
+        packages,
+        structures::{AnsPackage, ServerMetadata},
+    },
 };
-use actix_web::Responder;
+use actix_web::{Error, Responder};
 use mime_guess::from_path;
 use paperclip::actix::{
     api_v2_operation, get, post,
-    web::{self, HttpResponse},
+    web::{self, HttpResponse, Json},
 };
 use std::{str::FromStr, vec};
 
@@ -39,71 +42,71 @@ async fn echo(req_body: String) -> impl Responder {
 }
 #[api_v2_operation]
 #[get("v1/settings/init")]
-async fn init() -> impl Responder {
+async fn init() -> Result<Json<AnsPackage>, Error> {
     let package = packages::init();
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 #[api_v2_operation]
 #[get("v1/input/{sensor}")]
-async fn get_sensor(sensor: web::Path<String>) -> impl Responder {
+async fn get_sensor(sensor: web::Path<String>) -> Result<Json<AnsPackage>, Error> {
     let package = packages::reading(packages::Sensors::from_str(&sensor.into_inner()).unwrap());
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 #[api_v2_operation]
 #[get("v1/output/userled/{userled}")]
-async fn get_led(userled: web::Path<String>) -> impl Responder {
+async fn get_led(userled: web::Path<String>) -> Result<Json<AnsPackage>, Error> {
     let package =
         packages::get_led(hardware_manager::UserLed::from_str(&userled.into_inner()).unwrap());
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 #[api_v2_operation]
 #[post("v1/output/userled/{userled}/{value}")]
-async fn post_led(path: web::Path<(String, bool)>) -> impl Responder {
+async fn post_led(path: web::Path<(String, bool)>) -> Result<Json<AnsPackage>, Error> {
     let (userled, value) = path.into_inner();
     let package = packages::set_led(
         hardware_manager::UserLed::from_str(userled.as_str()).unwrap(),
         value,
     );
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 #[api_v2_operation]
 #[post("v1/output/neopixel/{red}/{green}/{blue}")]
-async fn post_neopixel(path: web::Path<(u8, u8, u8)>) -> impl Responder {
+async fn post_neopixel(path: web::Path<(u8, u8, u8)>) -> Result<Json<AnsPackage>, Error> {
     let (red, green, blue) = path.into_inner();
     let package = packages::set_neopixel(vec![[red, green, blue]]);
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 #[api_v2_operation]
 #[post("v1/output/pwm/{channel}/{value}")]
-async fn post_pwm(path: web::Path<(String, u16)>) -> impl Responder {
+async fn post_pwm(path: web::Path<(String, u16)>) -> Result<Json<AnsPackage>, Error> {
     let (channel, value) = path.into_inner();
     let package = packages::pwm_channel_value(
         hardware_manager::PwmChannel::from_str(channel.as_str()).unwrap(),
         value,
     );
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 #[api_v2_operation]
 #[post("v1/output/pwm/enable/{bool}")]
-async fn post_pwm_enable(path: web::Path<bool>) -> impl Responder {
+async fn post_pwm_enable(path: web::Path<bool>) -> Result<Json<AnsPackage>, Error> {
     let bool = path.into_inner();
     let package = packages::pwm_enable(bool);
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 
 #[api_v2_operation]
 #[post("v1/output/pwm/frequency/{frequency}")]
-async fn post_pwm_frequency(path: web::Path<f32>) -> impl Responder {
+async fn post_pwm_frequency(path: web::Path<f32>) -> Result<Json<AnsPackage>, Error> {
     let frequency = path.into_inner();
     let package = packages::set_pwm_freq_hz(frequency);
     hardware_manager::pwm_enable(true);
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
 
 /// The "register_service" route is used by BlueOS extensions manager
 #[api_v2_operation]
 #[get("register_service")]
-async fn get_server_metadata() -> impl Responder {
+async fn get_server_metadata() -> Result<Json<ServerMetadata>, Error> {
     let package = ServerMetadata::default();
-    HttpResponse::Ok().json(package)
+    Ok(Json(package))
 }
