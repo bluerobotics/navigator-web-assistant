17b2514922217da963fdcb9e4f6d8a2dccab7f29
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-10-12 14:44:06 -0300

    src : cli : Moved datalogger to hardware_manager

--- src/cli.rs
+++ src/cli.rs
@@ -1,22 +1,4 @@
-use crate::data_logger::DataLogger;
 use clap::{Arg, Command};
-use navigator_rs::Navigator;
-use std::thread::sleep;
-use std::time::Duration;
-
-pub fn start_logging(directory: &str, filename: &str) {
-    let mut nav = Navigator::new();
-    nav.init();
-
-    let file_path = format!("{}/{}", directory, filename);
-    let mut logger = DataLogger::new(&file_path).expect("Failed to create/open CSV file");
-
-    loop {
-        let sensors_data = nav.read_all();
-        logger.log_data(&sensors_data).expect("Failed to log data");
-        sleep(Duration::from_millis(1000));
-    }
-}
 
 pub fn parse_args() -> (String, String) {
     let matches = Command::new("Data Logger")
