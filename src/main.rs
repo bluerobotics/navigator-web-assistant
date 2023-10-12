b24374e69b2fbcf22692d93200a89f2dd919315f
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-10-12 14:37:17 -0300

    src: main: Re-enabled cli and datalogger services

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+mod cli;
+mod data_logger;
 mod hardware_manager;
 mod logger;
 mod server;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let (directory, filename) = cli::parse_args();
+
     logger::init();
 
     hardware_manager::init();
 
-    hardware_manager::init_auto_reading();
+    hardware_manager::init_monitor(500);
+
+    hardware_manager::init_datalogger(60000, directory, filename);
 
     server::run().await.unwrap();
 
