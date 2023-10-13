3872b266ea5fae2889c463ec6d8f0c00b59e9580
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-10-13 11:06:07 -0300

    src: main: Add enable and update rate to datalogger service

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,21 @@ mod server;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let (directory, filename) = cli::parse_args();
-
+    let (directory, filename, rate, enable) = cli::parse_args();
+    println!(
+        "Starting service with: {} {} {} {}",
+        directory, filename, rate, enable
+    );
     logger::init();
 
     hardware_manager::init();
 
     hardware_manager::init_monitor(500);
 
-    hardware_manager::init_datalogger(60000, directory, filename);
+    if enable {
+        hardware_manager::init_datalogger(rate, directory, filename);
+        log::info!("starting datalogger...");
+    }
 
     server::run().await.unwrap();
 
