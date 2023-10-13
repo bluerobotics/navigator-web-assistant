389b124f203ed6098ac9ff1ef1db8bc209902a25
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-10-13 11:08:35 -0300

    src: cli: Minor cleanup and reword to explicit datalogger related inputs

--- src/cli.rs
+++ src/cli.rs
@@ -1,35 +1,36 @@
 use clap::{Arg, Command};
 
 pub fn parse_args() -> (String, String) {
-    let matches = Command::new("Data Logger")
+    let matches = Command::new("Nagivator Assistant")
         .version("1.0")
         .author("BlueRobotics")
-        .about("Logs sensor data to a CSV file")
+        .about("Start your navigator assistant server")
         .arg(
-            Arg::new("directory")
+            Arg::new("datalogger_directory")
                 .short('d')
                 .long("directory")
-                .value_name("DIRECTORY")
                 .required(false),
         )
         .arg(
-            Arg::new("filename")
+            Arg::new("datalogger_filename")
                 .short('f')
                 .long("filename")
-                .value_name("FILENAME")
+                .required(false),
+        )
                 .required(false),
         )
         .get_matches();
 
-    let directory = matches
-        .get_one::<String>("directory")
+    let datalogger_directory = matches
+        .get_one::<String>("datalogger_directory")
         .map(|d| d.to_string())
-        .unwrap_or(".".to_string());
+        .unwrap_or("./".to_string());
 
-    let filename = matches
-        .get_one::<String>("filename")
+    let datalogger_filename = matches
+        .get_one::<String>("datalogger_filename")
         .map(|f| f.to_string())
         .unwrap_or("data.csv".to_string());
 
-    (directory, filename)
+        datalogger_directory,
+        datalogger_filename,
 }
