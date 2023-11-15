aba2515e620d5663c9c289c9b0cac425298f6f27
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-11-15 13:16:07 -0300

    src: cli: Add monitor settings and settings structures

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,21 @@
 use clap::{Arg, Command};
 
-pub fn parse_args() -> (String, String, u64, bool) {
-    let matches = Command::new("Nagivator Assistant")
+#[derive(Debug)]
+pub struct DataloggerSettings {
+    pub directory: String,
+    pub filename: String,
+    pub rate: u64,
+    pub enable: bool,
+}
+
+#[derive(Debug)]
+pub struct MonitorSettings {
+    pub rate: u64,
+    pub enable: bool,
+}
+
+pub fn parse_args() -> (DataloggerSettings, MonitorSettings) {
+    let matches = Command::new("Navigator Assistant")
         .version("1.0")
         .author("BlueRobotics")
         .about("Start your navigator assistant server")
@@ -23,8 +37,19 @@ pub fn parse_args() -> (String, String, u64, bool) {
         )
         .arg(
             Arg::new("datalogger_enable")
-                .short('e')
-                .long("enable")
+                .long("datalogger-enable")
+                .value_parser(clap::value_parser!(bool))
+                .required(false),
+        )
+        .arg(
+            Arg::new("monitor_rate")
+                .long("monitor-rate")
+                .value_parser(clap::value_parser!(u64))
+                .required(false),
+        )
+        .arg(
+            Arg::new("monitor_enable")
+                .long("monitor-enable")
                 .value_parser(clap::value_parser!(bool))
                 .required(false),
         )
@@ -50,10 +75,27 @@ pub fn parse_args() -> (String, String, u64, bool) {
         .copied()
         .unwrap_or(false);
 
-    (
-        datalogger_directory,
-        datalogger_filename,
-        datalogger_rate,
-        datalogger_enable,
-    )
+    let monitor_rate = matches
+        .get_one::<u64>("monitor_rate")
+        .copied()
+        .unwrap_or(10);
+
+    let monitor_enable = matches
+        .get_one::<bool>("monitor_enable")
+        .copied()
+        .unwrap_or(false);
+
+    let datalogger_settings = DataloggerSettings {
+        directory: datalogger_directory,
+        filename: datalogger_filename,
+        rate: datalogger_rate,
+        enable: datalogger_enable,
+    };
+
+    let monitor_settings = MonitorSettings {
+        rate: monitor_rate,
+        enable: monitor_enable,
+    };
+
+    (datalogger_settings, monitor_settings)
 }
